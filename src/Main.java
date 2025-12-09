b8f4ab66b108b8f4849d52cdb055f73c15e35a38
Philskillz <philipp.nikos@gmail.com>
2025-12-09 18:46:48 +0100

    pre

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Main {
     private final String CODE = ""+
 //            "func int fib(int n) { if (n == 0) { return 0; } else if (n == 1) { return 1; } else { return fib(n - 1) + fib(n - 2); } } print(fib(3));";
-        "func void t() {return;}";
+        "HelloWorld(\"print\");";
     // func
 
     public Main() {
